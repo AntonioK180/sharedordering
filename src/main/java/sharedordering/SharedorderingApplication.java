8e24f77b7e9d37a4ef8913ed79804915587e0f32
AntonioK180 <antonio.kasabov@gmail.com>
2022-01-06 20:34:54 +0200

    Initialize Spring Boot Application.

---
+++ src/main/java/sharedordering/SharedorderingApplication.java
@@ -0,0 +1,13 @@
+package sharedordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedorderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedorderingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sharedordering/SharedorderingApplicationTests.java
@@ -0,0 +1,13 @@
+package sharedordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedorderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
