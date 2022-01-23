580752973b25873c57189ebe54cb1c1c213ddcb9
AntonioK180 <antonio.kasabov@gmail.com>
2022-01-23 10:56:35 +0200

    Configure routerOutlet. Set up Angular Material.

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddOrderFormComponent } from './components/add-order-form/add-order-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'neworder', component: AddOrderFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderItemComponent } from './components/order-item/order-item.component';
 import { AddOrderFormComponent } from './components/add-order-form/add-order-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrderItemComponent,
-    AddOrderFormComponent
+    AddOrderFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
